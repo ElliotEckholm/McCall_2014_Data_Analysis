d050629d8f1655fc2700665767b7cee08147f374
Elliot Eckholm <ellioteckholm@Elliots-MacBook-Pro.local>
2017-01-08 10:49:52 -0800

    Initial Commit

---
+++ readMccallData/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  readMccallData
+//
+//  Created by Elliot Eckholm on 1/8/17.
+//  Copyright © 2017 Elliot Eckholm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
